fdb1bb9ebbd8dcbac81ffc8ae3e3965d1c33fa8e
pyxiis <47072520+eniraa@users.noreply.github.com>
2021-11-14 19:02:07 -0500

    Remove proxy container
    This will later be replaced with some træfik and caddy thing.

--- proxy/up.rb
+++
@@ -1,9 +0,0 @@
-# create certificates
-`mkdir -p proxy/nginx/letsencrypt`
-`apk add certbot`
-`certbot certonly --standalone -d eji.moe --email info@eji.moe -w /var/www/_letsencrypt -n --agree-tos --force-renewal`
-`echo -e '#!/bin/sh\nnginx -t && rc-service nginx reload' | sudo tee /etc/letsencrypt/renewal-hooks/post/nginx-reload.sh`
-`chmod a+x /etc/letsencrypt/renewal-hooks/post/nginx-reload.sh`
-
-# start app
-`docker-compose -f proxy/docker-compose.yml up --detach`
